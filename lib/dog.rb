4e300b2939a3f49b4a7501c63bee0daca532d7ef
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-20 00:50:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,6 +54,12 @@ class Dog
   end
 
   def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      AND breed = ?
+      SQL
     row = DB[:conn].execute(sql, name, breed)
     if !row.empty?
       dog = self.new_from_db(row[0])
